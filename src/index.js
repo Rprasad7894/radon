56e9a89f6a37195fc556d60096a10b74567e6290
functionup-demo <prn07361@gmail.com>
2022-06-08 15:18:23 +0530

    session/mongo-session1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const route = require('./routes/route.js');
+const express = require("express");
+var bodyParser = require("body-parser");
+
+const route = require("./routes/route.js");
+const { default: mongoose } = require("mongoose");
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', route);
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+mongoose
+  .connect(
+    "mongodb+srv://RNP:*****@cluster0.ihohe.mongodb.net/RNP-DB?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => console.log("MongoDb connected"))
+  .catch((err) => console.log(err));
+
+app.use("/", route);
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express app running on port " + (process.env.PORT || 3000));
 });
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,52 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const UserModel= require("../models/userModel.js")
+const UserController= require("../controllers/userController")
 
-let players =
-   [
-       {
-           "name": "manish",
-           "dob": "1/1/1995",
-           "gender": "male",
-           "city": "jalandhar",
-           "sports": [
-               "swimming"
-           ]
-       },
-       {
-           "name": "gopal",
-           "dob": "1/09/1995",
-           "gender": "male",
-           "city": "delhi",
-           "sports": [
-               "soccer"
-           ],
-       },
-       {
-           "name": "lokesh",
-           "dob": "1/1/1990",
-           "gender": "male",
-           "city": "mumbai",
-           "sports": [
-               "soccer"
-           ],
-       },
-   ]
- 
-   router.post('/players', function (req, res) {
- 
-       //LOGIC
+router.get("/test-me", function (req, res) {
+    res.send("My first ever api!")
+})
 
-       if ((players.find((data)=>(data.name==req.body.name)))==undefined)
-       {
-           players.push(req.body)
-           res.send(  { data: players , status: true }  )
-       }
-       else{
-            res.send("Player Name "+ req.body.name +" --- already Exist in DataBase")
-       }
-       
-       console.log(players)
-   })
+router.post("/createUser", UserController.createUser  )
 
+router.get("/getUsersData", UserController.getUsersData)
 
 module.exports = router;
